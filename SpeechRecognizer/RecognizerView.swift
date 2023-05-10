d8c8b4b6b148a274d0aa40a29b8e3519090272a3
kenta-kawamura <ffr.wakamononosubete@gmail.com>
2023-05-10 22:47:42 +0900

    音声認識完成

--- SpeechRecognizer/RecognizerView.swift
+++ SpeechRecognizer/RecognizerView.swift
@@ -7,37 +7,57 @@
 
 import SwiftUI
 
-struct recognizerView: View {
-    @State private var recognizedText = ""
-
+struct RecognizerView: View {
+    @ObservedObject private var viewModel = RecognizerViewModel()
     var body: some View {
         ZStack {
             VStack {
-                Text(recognizedText)
+                Color.init(.init(gray: 0.2, alpha: 1))
                     .frame(maxWidth: .infinity)
                     .frame(height: UIScreen.main.bounds.height / 2)
-                    .background(Color.init(.init(gray: 0.2, alpha: 1)))
+                    .overlay {
+                        Text(viewModel.speechResult)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                    }
                     .padding(.horizontal, 32)
                     .padding(.top, 64)
                 Spacer()
                 Button {
-                    print("ボタン")
+                    if viewModel.isRecognizing {
+                        viewModel.stopRecognition()
+                    } else {
+                        viewModel.checkEnableRecognition()
+                    }
                 } label: {
                     Circle()
+                        .strokeBorder(.white, lineWidth: 4, antialiased: true)
                         .frame(width: 64, height:64)
                         .foregroundColor(.clear)
-                        .
+                        .overlay {
+                            if !viewModel.isRecognizing {
+                                Circle()
+                                    .foregroundColor(.red)
+                                    .padding(8)
+                            } else {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .foregroundColor(.red)
+                                    .frame(width: 32, height: 32)
+                            }
+                        }
                 }
-
+                .padding(.bottom, 32)
+                .animation(.default, value: viewModel.isRecognizing)
             }
         }
-//        .frame(maxWidth: .infinity, minHeight: .infinity)
         .background(.black)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RecognizerView_Previews: PreviewProvider {
     static var previews: some View {
-        recognizerView()
+        RecognizerView()
     }
 }
--- SpeechRecognizer/RecognizerViewModel.swift
+++ SpeechRecognizer/RecognizerViewModel.swift
@@ -5,4 +5,177 @@
 //  Created by 河村健太 on 2023/05/07.
 //
 
-import Foundation
+import SwiftUI
+import Speech
+import AVFoundation
+import Combine
+
+final class RecognizerViewModel: NSObject, ObservableObject {
+    @Published var speechResult = ""
+    @Published var isRecognizing = false
+    @Published var errorAlertFlag = false
+
+    private let speechRecognizer = SFSpeechRecognizer(locale: .init(identifier: "ja_JP"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private var audioEngin: AVAudioEngine?
+
+    override init() {
+        super.init()
+        speechRecognizer?.delegate = self
+    }
+
+    func checkEnableRecognition() {
+        Task {
+            guard await checkMicrophoneAuthorization() else {
+                errorAlertFlag = true
+                return
+            }
+            checkRecognizerAuthorization { isAuthorized in
+                if isAuthorized {
+                    self.startRecognition()
+                } else {
+                    self.errorAlertFlag = true
+                }
+            }
+        }
+    }
+
+    private func checkMicrophoneAuthorization() async -> Bool {
+        switch AVCaptureDevice.authorizationStatus(for: .audio) {
+            case .notDetermined:
+                return await AVCaptureDevice.requestAccess(for: .audio)
+            case .restricted, .denied:
+                return false
+            case .authorized:
+                return true
+            @unknown default:
+                fatalError()
+        }
+    }
+
+    private func checkRecognizerAuthorization(didCheck: @escaping ((Bool) -> Void)) {
+        switch SFSpeechRecognizer.authorizationStatus() {
+            case .notDetermined:
+                SFSpeechRecognizer.requestAuthorization { status in
+                    didCheck(status == .authorized)
+                }
+            case .denied, .restricted:
+                didCheck(false)
+            case .authorized:
+                didCheck(true)
+            @unknown default:
+                fatalError()
+        }
+    }
+
+    private func startRecognition() {
+        // リセットのため終了時の処理を呼ぶ
+        stopRecognition()
+
+        // オーディオセッションの変更
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.record, mode: .measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            stopRecognition()
+            return
+        }
+
+        // 音声認識リクエストの作成
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else {
+            stopRecognition()
+            return
+        }
+        // リアルタイムで結果を返すか
+        recognitionRequest.shouldReportPartialResults = true
+        // 音声データをデバイス上だけで認識を行うか、falseでネットを介して結果を取得
+        recognitionRequest.requiresOnDeviceRecognition = false
+        // 句読点を結果に含むか
+        if #available(iOS 16, *) {
+            recognitionRequest.addsPunctuation = true
+        }
+
+        //音声認識タスクの生成（delegateじゃなくコールバックでハンドリングも可能）
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, delegate: self)
+
+        // マイク入力ノードの準備
+        audioEngin = AVAudioEngine()
+        let inputNode = audioEngin?.inputNode
+        let recordingFormat = inputNode?.outputFormat(forBus: 0)
+        // ノード出力の監視
+        inputNode?.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat, block: { buffer, when in
+            // 録音データをrecognizerに渡す
+            recognitionRequest.append(buffer)
+        })
+        // エンジンの用意
+        audioEngin?.prepare()
+        do {
+            // 音声取得スタート
+            try audioEngin?.start()
+            Task { @MainActor in
+                speechResult = ""
+                isRecognizing = true
+            }
+        } catch {
+            stopRecognition()
+        }
+    }
+
+    func stopRecognition() {
+        // 音声の取得と認識に使うものをリセットする
+        audioEngin?.stop()
+        audioEngin?.inputNode.removeTap(onBus: 0)
+        recognitionRequest?.endAudio()
+        recognitionRequest = nil
+        recognitionTask?.finish()
+        recognitionTask = nil
+        let audioSession = AVAudioSession.sharedInstance()
+        try? audioSession.setCategory(.soloAmbient, mode: .default)
+        try? audioSession.setActive(false)
+        Task { @MainActor in
+            isRecognizing = false
+        }
+    }
+}
+
+extension RecognizerViewModel: SFSpeechRecognitionTaskDelegate, SFSpeechRecognizerDelegate {
+    func speechRecognitionDidDetectSpeech(_ task: SFSpeechRecognitionTask) {
+        // 初めて音声を検出
+    }
+
+    func speechRecognitionTaskFinishedReadingAudio(_ task: SFSpeechRecognitionTask) {
+        // オーディオ入力が終わった
+        stopRecognition()
+    }
+
+    func speechRecognitionTask(_ task: SFSpeechRecognitionTask, didHypothesizeTranscription transcription: SFTranscription) {
+        // 中間結果を受け取る
+        speechResult = transcription.formattedString
+    }
+
+    func speechRecognitionTask(_ task: SFSpeechRecognitionTask, didFinishRecognition recognitionResult: SFSpeechRecognitionResult) {
+        // 最後の音声が認識された
+        // リアルタイムではなく、最終的な認識結果が欲しい場合はこちらから取得
+//        speechResult = recognitionResult.bestTranscription.formattedString
+    }
+
+    func speechRecognitionTask(_ task: SFSpeechRecognitionTask, didFinishSuccessfully successfully: Bool) {
+        // 音声認識が成功で終了したか
+        stopRecognition()
+    }
+
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        // 音声認識の可用性が変更された
+        if !available {
+            stopRecognition()
+        }
+    }
+
+    func speechRecognitionTaskWasCancelled(_ task: SFSpeechRecognitionTask) {
+        // 音声認識タスクがキャンセルされた
+        stopRecognition()
+    }
+}
